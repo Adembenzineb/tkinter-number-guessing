bf0ac1d0d8545b455e6691ecdf890af1630ddfad
Adem Ben Zineb <adem.benzineb.dev@gmail.com>
2025-06-08 18:00:50 +0200

    Add the range in the interface and some tests
    Add the range getting elements and the start button function
    with controling the input

---
+++ game.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.geometry('350x300')
+root.title("Number guessing game")
+
+l1 = Label(root ,  text="select the range")
+l1.grid(column=2,row=0)
+
+ln = Label(root ,  text="select minimum")
+ln.grid(column=0,row=1)
+txtn = Entry(root,width=5)
+txtn.grid(column = 1,row = 1)
+
+lm = Label(root ,  text="select maximum")
+lm.grid(column=0,row=2)
+txtm = Entry(root,width=5)
+txtm.grid(column = 1,row = 2)
+
+def play():
+    n = txtn.get()
+    m = txtm.get()
+
+    if n == "" or m == "" :
+        messagebox.showerror("Invalid Input", "Please enter a number!")
+    elif not(n.isdecimal) or not(m.isdecimal) :
+        messagebox.showerror("Invalid Input", "Please enter a number!")
+    else:
+        max = int(m)
+        min = int(n)
+
+
+bts = Button(root,text="Start",command= play )
+bts.grid(column=2,row=3)
+
+
+
+btq = Button(root ,text="Quit",command=root.destroy)
+btq.grid(column=2,row=20)
+root.mainloop()
